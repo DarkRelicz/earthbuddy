d606086c9ac26047b2249b4d2a8565ed13c4d8ab
CloudKai <kaiming_01@live.com>
2025-06-16 23:14:31 +0800

    Try yfinance

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+import yfinance as yf
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return {"message": "Hello from Render"}
+@app.get("/esg/{ticker}")
+def get_esg(ticker: str):
+    ticker_obj = yf.Ticker(ticker)
+    esg_data = ticker_obj.sustainability
+    if esg_data is not None:
+        return esg_data.to_dict()
+    else:
+        return {"error": "No ESG data found"}
