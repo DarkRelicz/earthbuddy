38139b2c7ea86908fd57d12bbbd5a8389bb5fd42
CloudKai <kaiming_01@live.com>
2025-06-18 02:29:48 +0800

    Change Logo URL

--- utils/getlogo.py
+++ utils/getlogo.py
@@ -11,7 +11,7 @@ def fetch_image_url(brand_name):
         str: The logo icon URL or a fallback message if unavailable.
     """
     brand_name = brand_name.lower() # Format brand name for URL
-    return f"https://cdn.brandfetch.io/{brand_name}.com?c=1idixjeVit8iMxTe5MU"
+    return f"cdn.brandfetch.io/{brand_name}.com/w/512/h/178/logo?c=1idixjeVit8iMxTe5MU"
     # response = requests.get(url)
     # if response.status_code == 200:
     #     data = response.json()
