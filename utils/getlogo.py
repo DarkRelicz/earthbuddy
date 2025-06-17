1830a9ef5b982ca0fd9eecdd4d922ad48e8fb0a3
CloudKai <kaiming_01@live.com>
2025-06-17 17:59:32 +0800

    Rollback logo link

--- utils/getlogo.py
+++ utils/getlogo.py
@@ -11,10 +11,10 @@ def fetch_image_url(brand_name):
         str: The logo icon URL or a fallback message if unavailable.
     """
     brand_name = brand_name.lower() # Format brand name for URL
-    url = f"https://api.brandfetch.io/v2/search/{brand_name}?c=1idixjeVit8iMxTe5MU"
-    response = requests.get(url)
-    if response.status_code == 200:
-        data = response.json()
-        if len(data) > 0 and "icon" in data[0]:  # Check if the first brand has an icon
-            return data[0]["icon"]  # Return the icon URL of the first brand
-    return "No image available"
+    return f"https://cdn.brandfetch.io/{brand_name}.com?c=1idixjeVit8iMxTe5MU"
+    # response = requests.get(url)
+    # if response.status_code == 200:
+    #     data = response.json()
+    #     if len(data) > 0 and "icon" in data[0]:  # Check if the first brand has an icon
+    #         return data[0]["icon"]  # Return the icon URL of the first brand
+    # return "No image available"
